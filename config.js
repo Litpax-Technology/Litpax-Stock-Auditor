a0b69c20bfa1b008557b7445bea3af41707e8d4a
Litpax-Technology <mis@litpaxtechnology.com>
2026-08-07 10:52:06 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// ============================================================
+// Apps Script Web App URL yahan paste karo (deploy ke baad mila hua)
+// Example: https://script.google.com/macros/s/AKfyc..../exec
+// ============================================================
+const API_URL = 'https://script.google.com/macros/s/AKfycbzsBDdHHDJv6kw8UNWB6Eo11kvrZGcZfKAARgavN-UrvZ1_vAtvZqs1tLtbT-3qzvjn/exec';
